148c727e7233dafe7618d9e07aeb4a1ae3e44e2f
Ibrahim Jabbar <ibrahimilu@gmail.com>
2018-09-01 00:36:49 -0700

    initialCommit

--- src/index.js
+++ src/index.js
@@ -1,16 +1,33 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import "./styles.css";
+class HelloUser extends React.Component {
+  constructor(props) {
+    super(props)
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+    this.state = {
+      username: 'Ibrahim Jabbar'
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+  }
+  handleChange(e) {
+    this.setState({
+      username: e.target.value
+    })
+  }
+  render() {
+    return (
+      <div>
+        Hello {this.state.username} <br />
+        Change Name:
+        <input
+          type="text"
+          value={this.state.username}
+          onChange={this.handleChange}
+        />
+      </div>
+    )
+  }
 }
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<HelloUser />, document.getElementById('root'));
